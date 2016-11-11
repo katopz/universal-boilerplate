84b2ea8af6c68e09bd19ab0aa1a43d82c6541c53
Todsaporn (katopz) Banjerdkit <katopz@gmail.com>
2016-11-11 18:37:04 +0700

    Workinf svg icon inline.

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,8 @@ import { browserHistory } from 'react-router'
 import Explore from '../components/Explore'
 import { resetErrorMessage } from '../actions'
 
+import { Appbar, Container } from 'muicss/react';
+
 class App extends Component {
   constructor(props) {
     super(props)
@@ -31,7 +33,7 @@ class App extends Component {
         <b>{errorMessage}</b>
         {' '}
         (<a href="#"
-            onClick={this.handleDismissClick}>
+          onClick={this.handleDismissClick}>
           Dismiss
         </a>)
       </p>
@@ -39,14 +41,26 @@ class App extends Component {
   }
 
   render() {
+    //const fb_icon = 'M608 192h160v-192h-160c-123.514 0-224 100.486-224 224v96h-128v192h128v512h192v-512h160l32-192h-192v-96c0-17.346 14.654-32 32-32z'
+    const icon = 'M15.7 13.3l-3.81-3.83A5.93 5.93 0 0 0 13 6c0-3.31-2.69-6-6-6S1 2.69 1 6s2.69 6 6 6c1.3 0 2.48-.41 3.47-1.11l3.83 3.81c.19.2.45.3.7.3.25 0 .52-.09.7-.3a.996.996 0 0 0 0-1.41v.01zM7 10.7c-2.59 0-4.7-2.11-4.7-4.7 0-2.59 2.11-4.7 4.7-4.7 2.59 0 4.7 2.11 4.7 4.7 0 2.59-2.11 4.7-4.7 4.7z'
     const { children, inputValue } = this.props
     return (
       <div>
-        <Explore value={inputValue}
-                 onChange={this.handleChange} />
-        <hr />
-        {this.renderErrorMessage()}
-        {children}
+        <Appbar>
+          ☰ Foo
+          <div className="mui--text-right">
+            <svg>
+              <path d={icon}></path>
+            </svg>
+          </div>
+        </Appbar>
+        <Container>
+          <Explore value={inputValue}
+            onChange={this.handleChange} />
+          <hr />
+          {this.renderErrorMessage()}
+          {children}
+        </Container>
       </div>
     )
   }
